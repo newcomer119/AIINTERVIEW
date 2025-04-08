32264a6dcee0d6087ca91215be59b28f3a2d6dd6
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-04-08 18:47:08 +0530

    fix the string error for deployement

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -8,7 +8,12 @@ const Page = async () => {
     <>
       <h3>Interview generation</h3>
 
-      <Agent userName={user?.name!} userId={user?.id} type="generate" />
+      <Agent
+        userName={user?.name || "Default Name"}
+        userId={user?.id || "Default ID"}
+        type="generate"
+      />
+      
     </>
   );
 };
